438c5889adeec708cf4d49f2224c9c912cb25f52
Jorge Fernando Barraza Zambada <jofer_bz20@hotmail.com>
2018-02-15 13:59:42 -0700

    Commit inicial

---
+++ Droid/AddContactoActivity.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace ejmeplo1.Droid
+{
+    [Activity(Label = "Add Contacto")]
+    public class AddContacto : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.AddContacto);
+
+            // Create your application here
+        }
+    }
+}
---
+++ Droid/MainActivity.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+
+namespace ejmeplo1.Droid
+{
+    [Activity(Label = "Menu principal", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            Button btnAddContacto = FindViewById<Button>(Resource.Id.btnAddContacto);
+            Button button = FindViewById<Button>(Resource.Id.myButton);
+
+            btnAddContacto.Click += delegate {
+                var activityAddContacto = new Intent(this, typeof(AddContacto));
+                activityAddContacto.PutExtra("MyData", "Data from Activity1");
+                StartActivity(activityAddContacto);
+            };
+
+            button.Click += delegate { button.Text = $"{count++} clicks!"; };
+        }
+    }
+}
+
