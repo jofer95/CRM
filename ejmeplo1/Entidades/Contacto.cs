3bc60b7f68810c6c6fdc933766d2951dffbe484e
Jorge Fernando Barraza Zambada <jofer_bz20@hotmail.com>
2018-02-21 13:04:38 -0700

    Creacion de la interfaz

---
+++ ejmeplo1/Entidades/Contacto.cs
@@ -0,0 +1,20 @@
+﻿using ejmeplo1.Enumeradores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejmeplo1
+{
+    public class Contacto
+    {
+        public int ContactoID { get; set; }
+        public String ApellidoPaterno { get; set; }
+        public String ApellidoMaterno { get; set; }
+        public String Nombre { get; set; }
+        public String Correo { get; set; }
+        public String Telefono { get; set; }
+        public TipoCliente TipoCliente { get; set; }
+    }
+}
---
+++ ejmeplo1/Interfaces/IContacto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejmeplo1.Interfaces
+{
+    public interface IContacto
+    {
+        List<Contacto> ObtenerContactos();
+        Contacto ObtenerContactoPorID(int contactoID);
+        void CrearContacto(Contacto contacto);
+        void ActualizarContacto(Contacto contacto);
+        void BorrarContactoPorID(int contactoID);
+    }
+}
