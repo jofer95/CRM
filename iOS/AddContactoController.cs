e5f1cf410ca51794a628d8ffaa11b1ed057c5e21
Jorge Fernando Barraza Zambada <jofer_bz20@hotmail.com>
2018-02-26 13:57:46 -0700

    Agregado el controller de add contacto

---
+++ iOS/AddContactoController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace ejmeplo1.iOS
+{
+    public partial class AddContactoController : UIViewController
+    {
+        public AddContactoController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
